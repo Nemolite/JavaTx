515e06525727d1c7121b8a8542396164760d725a
shumcoder <g16052015@mail.ru>
2023-10-09 13:40:09 +0300

    interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+// Итерфейсы
+
+interface MyInter{
+    void myfanc();
+}
+
+class MyClass implements MyInter{
+    public void myfanc(){
+        System.out.println("MyClass->MyInter");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+        // --
+        // MyInter mi = new MyInter();
+
+       MyClass mc = new MyClass();
+       mc.myfanc();
 
     }
 }
