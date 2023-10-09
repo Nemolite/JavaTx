0c945f4d919cf34a75b720748321ad9b639336f7
shumcoder <g16052015@mail.ru>
2023-10-09 13:50:58 +0300

    poly

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,40 @@
 
 interface MyInter{
     void myfanc();
+
+    default void print(){
+
+        System.out.println("default method");
+    }
 }
 
 class MyClass implements MyInter{
     public void myfanc(){
         System.out.println("MyClass->MyInter");
+    }
+}
+
+class MyClass2 implements  MyInter{
+    public void myfanc(){
+        System.out.println("MyClass2->MyInter");
+    }
+}
+
+////////////////////////////////
+
+interface Inter {
+    double method(double a,double b);
+}
+
+class Simple1 implements  Inter{
+    public double method(double a,double b){
+        return a * b;
+    }
+}
+
+class Simple2 implements  Inter{
+    public double method(double a,double b){
+        return a / b;
     }
 }
 
@@ -17,6 +46,16 @@ public class Main {
 
        MyClass mc = new MyClass();
        mc.myfanc();
+
+        MyClass2 mc2 = new MyClass2();
+        mc2.print();
+
+        ///////
+        Inter in;
+        in = new Simple1();
+        System.out.println(in.method(4,2));
+        in = new Simple2();
+        System.out.println(in.method(4,2));
 
     }
 }
